0c903f4e15bb487a5eb1d8adba38fc00826b715d
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-03 10:04:28 -0700

    Give me some power

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def pow(a: int, b: int):
+    '''
+        pow - computes a to the power of b and return the value.
+        Return: the value of a to the power of b.
+    '''
+    return a ** b
