703e087a15d41435c5feb43e922b3fc2b3cf8821
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-03 09:47:39 -0700

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str: str):
+    """
+        uppercase - prints a string in uppercase followed by a new line.
+        @str: the target string
+    """
     var = ''
     for i in range(len(str)):
         if ord('a') <= ord(str[i]) <= ord('z'):
