bf8ecbf16c847623a491212a4266e89e31e3ccc7
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-03 09:15:44 -0700

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-def islower(c):
+def islower(c: str) -> bool:
+    '''
+        islower - checks for lowercase character.
+        @c: Single character string to check.
+        Return: True if c is a lowercase letter, False otherwise.
+    '''
     if ord('a') <= ord(c) <= ord('z'):
         return True
     return False
