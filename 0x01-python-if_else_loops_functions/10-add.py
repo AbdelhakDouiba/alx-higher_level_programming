9e72c9ec0f17c46bb1ff6cec3428cff63dff066e
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-03 09:59:39 -0700

    do this for me : a + b

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def add(a: int, b: int) -> int:
+    '''
+        add - adds two integers and returns the result.
+        Return: the result of the addition
+    '''
+    return a + b
