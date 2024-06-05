68a6770a76747b84fa156d9db02a7bc5ee3b17a0
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-05 07:23:29 -0700

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,4 +3,4 @@ def replace_in_list(my_list: list, idx: int, element: int) -> list:
     if 0 <= idx < len(my_list):
         my_list[idx] = element
         return my_list
-    return None
+    return my_list
