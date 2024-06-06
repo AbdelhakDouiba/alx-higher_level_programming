19f460d443fa1bae0e13d53f223784a16ad6b5cd
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-06 08:39:34 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix: list = []) -> list:
     new_matrix = []
     for row in matrix:
-        new_matrix += [(lambda x: [i * 2 for i in x])(row)]
+        new_matrix += [[i * 2 for i in row]]
     return new_matrix
