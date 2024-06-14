5e27eb010852291944f81eeb15e2a900f1dcef71
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-13 17:34:45 -0700

    Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value) -> bool:
+    try:
+        print("{:d}".format(value))
+        return True
+    except BaseException as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
