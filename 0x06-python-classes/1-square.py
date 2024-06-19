14586bad59c79861a8cd6185218f7c0938132a14
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-19 06:36:17 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Square with size'''
+
+
+class Square:
+    '''Square class with size'''
+    def __init__(self, size):
+        '''init constructor
+
+            initialize a private attribute called "size"'''
+        self.__size = size
