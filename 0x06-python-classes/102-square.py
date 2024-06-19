d12430ca0e2c8110c5ffb9e4e85e3377b20f4142
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-19 11:47:19 -0700

    Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+'''
+Square class with some operations
+'''
+
+
+class Square:
+    '''Square class with some operations to claculate the area and
+       store the size of the square
+
+        Attributes:
+            __size: size of the sqaure
+    '''
+    def __init__(self, size: int = 0) -> None:
+        '''Square class
+
+            Args:
+                size: size of the sqaure
+        '''
+        self.size = size
+
+    @property
+    def size(self):
+        '''size getter'''
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        '''size setter'''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''Calculate the area of the square
+
+            Returns:
+                the area of the square
+        '''
+        return self.__size * self.__size
+
+    def __eq__(self, other):
+        '''equal to'''
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        '''not equal to'''
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        '''greater than'''
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        '''greater or equal to'''
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        '''lesser than'''
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        '''lesser or equal to'''
+        return self.area() <= other.area()
