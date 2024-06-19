4faf89d8cfb7cbb7b124b4cc59fee67eba1aceed
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-19 11:26:27 -0700

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -62,11 +62,11 @@ class Square:
         if self.__size <= 0:
             print()
         else:
-            print(" " * self.__position[0], end="")
             print("\n" * self.__position[1], end="")
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
+
     def __str__(self):
         '''the class string'''
         return_str = ""
