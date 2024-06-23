a6f16e54bf96e55928f42aeb520de937801c8bc6
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-23 07:31:37 -0700

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,11 +2,15 @@
 '''
 This is add_integer module
 define a simple add function
+---> Expected inputs types : integer or float.
 '''
 
 
 def add_integer(a, b=98) -> int:
-    '''Calculate the addition of a and b'''
+    '''Calculate the addition of a and b
+        Returns: The addition of a and b (a + b)
+    '''
+
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
