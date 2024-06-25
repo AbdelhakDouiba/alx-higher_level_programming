c633f6962cda636c6eaa7730f0a3d3129c160192
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-06-25 08:14:36 -0700

    I can be anything

--- 0x08-python-more_classes/7-main.py
+++ 0x08-python-more_classes/7-main.py
@@ -3,7 +3,7 @@ Rectangle = __import__('7-rectangle').Rectangle
 
 my_rectangle_1 = Rectangle(8, 4)
 print(my_rectangle_1)
-print("--")
+print("---")
 my_rectangle_1.print_symbol = "&"
 print(my_rectangle_1)
 print("--")
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     '''Defining the empty class'''
 
     number_of_instances = 0
-    print_symbol = "##"
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         '''the init method'''
