02f61809ee042d3169d76a4812e63770aeec70de
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-07-13 08:34:41 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,11 +15,13 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 def add_json():
     """adds all arguments to a Python list, and then save them to a file"""
 
+    old_data = []
+
     if path.exists("./add_item.json"):
         try:
             old_data = load_from_json_file("add_item.json")
         except BaseException:
-            old_data = []
+            pass
 
     new_data = old_data + argv[1:]
 
