5b190463b31ebdfa307820aae4f726018495253a
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-07-13 07:43:26 -0700

    See, JSON is a legend

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is "5-save_to_json_file.py" Module
+"""
+
+
+def save_to_json_file(my_obj, filename: str):
+    """writes an Object to a text file, using a JSON representation"""
+
+    import json
+
+
+    with open(filename, "w", encoding="UTF-8") as json_file:
+        json.dump(my_obj, json_file)
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is "6-load_from_json_file" Module
+"""
+
+
+def load_from_json_file(filename):
+    """creates an Object from a 'JSON file'"""
+
+    
