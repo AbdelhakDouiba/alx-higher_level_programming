79ea69b8310628f235981683236cdb575f7e1f32
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-07-13 12:30:51 -0700

    Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+This is "101-stats" module
+"""
+
+
+def print_data(size, st):
+    print("File size: {}".format(size))
+
+    for status, occ in sorted(st.items()):
+        if occ != 0:
+            print("{:d}: {:d}".format(int(status), occ))
+
+
+def stats():
+    """reads stdin line by line and computes metrics"""
+
+    from sys import stdin
+
+    status = {
+            "200": 0,
+            "301": 0,
+            "400": 0,
+            "401": 0,
+            "403": 0,
+            "404": 0,
+            "405": 0,
+            "500": 0
+        }
+    count = 1
+    size = 0
+
+    try:
+        for line in stdin:
+            code = line.split()[-2]
+            size += int(line.split()[-1])
+
+            if code in status:
+                status[code] = status[code] + 1
+            else:
+                status[code] = 0
+
+            if count == 10:
+                print_data(size, status)
+
+                count = 1
+
+            count = count + 1
+    except KeyboardInterrupt:
+        print_data(size, status)
+        raise
+
+
+if __name__ == "__main__":
+    stats()
