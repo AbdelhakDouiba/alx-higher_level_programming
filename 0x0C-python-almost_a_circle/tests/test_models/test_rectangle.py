81d47f8c4076d79e1c195ea0c9b785b9a43c53cc
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2024-07-15 06:35:57 -0700

    If its not tested it doesnt work

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,6 +14,19 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """Testing Rectangle Class"""
 
+    @staticmethod
+    def evaluate(obj, method):
+        """return value"""
+        oldstdout = sys.stdout
+        newstdout = io.StringIO()
+        sys.stdout = newstdout
+        if method == "print":
+            print(obj)
+        else:
+            obj.display()
+        sys.stdout = oldstdout
+        return newstdout.getvalue()
+
     def setUp(self):
         """Setting Up all TestCases"""
         Base._Base__nb_objects = 0
@@ -27,18 +40,21 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rec.width, 1)
         self.assertEqual(rec.height, 2)
         self.assertEqual(rec.x, 3)
-        r = Rectangle(1, 3)
-        self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 3)
-        self.assertEqual(r.x, 0)
-        self.assertEqual(r.y, 0)
-        self.assertEqual(r.id, 6)
         r = Rectangle(1, 2, 1, 1, 88)
         self.assertEqual(r.width, 1)
         self.assertEqual(r.height, 2)
         self.assertEqual(r.x, 1)
         self.assertEqual(r.y, 1)
         self.assertEqual(r.id, 88)
+
+    def test_v_input(self):
+        """testing valid input"""
+        Base._Base__nb_objects = 0
+        r = Rectangle(1, 3)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 3)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
 
     def test_invalid_input(self):
         """testing invalid input"""
@@ -87,18 +103,34 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.area(), 2)
 
     def test_str(self):
+        """testing object string"""
         r = Rectangle(1, 2, 0, 0, 5)
-
         st = "[Rectangle] (5) 0/0 - 1/2"
-
-        oldstdout = sys.stdout
-        newstdout = io.StringIO()
-        sys.stdout = newstdout
-
-        print(r)
-
-        sys.stdout = oldstdout
-
-        output = newstdout.getvalue()
-
+        output = self.evaluate(r, "print")
         self.assertEqual(st.rstrip(), output.rstrip())
+
+    def test_display(self):
+        """testing display method"""
+        r = Rectangle(2, 3, 0, 0)
+        output = self.evaluate(r, "display")
+        val = "##\n##\n##\n"
+        self.assertEqual(output, val)
+        r = Rectangle(2, 3)
+        output = self.evaluate(r, "display")
+        val = "##\n##\n##\n"
+        self.assertEqual(output, val)
+        r = Rectangle(2, 3, 1)
+        output = self.evaluate(r, "display")
+        val = " ##\n ##\n ##\n"
+        self.assertEqual(output, val)
+
+    def test_to_dictionary(self):
+        """testing to_dictionary method"""
+        r = Rectangle(2, 3)
+        val = {"id": r.id,
+                "width": r.width,
+                "height": r.height,
+                "x": r.x,
+                "y": r.y
+                }
+        self.assertEqual(r.to_dictionary(), val)
